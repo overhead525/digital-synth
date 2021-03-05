35ed8eddcbc1ba6f12b25c5796c69e0e19ecb391
overhead525 <ritchelcousar@outlook.com>
2021-03-05 08:57:08 -0500

    added a mechanism to determine if screen is wide enough to effectively display the entire Synthesizer GUI

---
+++ src/components/ScreenTooSmall.js
@@ -0,0 +1,5 @@
+const ScreenTooSmall = () => {
+  return <div>Sorry the screen is too small to effectively use this Synthesizer. Must be wider than 1080px</div>
+}
+
+export default ScreenTooSmall;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
 import './style';
 import 'normalize.css'
 import SynthGUI from "./components/SynthGUI";
+import ScreenTooSmall from "./components/ScreenTooSmall";
 
 export default function App() {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+	window.onresize = () => setWindowWidth(window.innerWidth);
+
+	const enoughScreenSpace = () => {
+		return window.innerWidth >= 1080 ? <SynthGUI /> : <ScreenTooSmall />
+	}
+
 	return (
 		<div>
-			<SynthGUI />
+			{enoughScreenSpace()}
 		</div>
 	);
 }
