d6ccb09054a89c129305b682726b9855c04c34df
overhead525 <ritchelcousar@outlook.com>
2021-03-11 13:38:13 -0500

    added some styling to the dropdown list to help the user understand what is going on

--- src/components/SynthGUI/TopMenu/selector.js
+++ src/components/SynthGUI/TopMenu/selector.js
@@ -1,5 +1,5 @@
 import { useState } from "preact/hooks";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 import { soundChoices } from "../../../contexts/SynthContexts";
 
@@ -8,12 +8,12 @@ const StyledSelectorContainer = styled.div`
 `;
 
 const StyledSelectorHeader = styled.div`
-  background-color: #83867F;
+  background-color: #83867f;
   width: 15rem;
   padding: 0.255rem 0;
   text-align: center;
 
-  box-shadow: inset 0px 0px 10px 0px #43D9D9;
+  box-shadow: inset 0px 0px 10px 0px #43d9d9;
 `;
 
 const StyledSelectorList = styled.div`
@@ -23,43 +23,52 @@ const StyledSelectorList = styled.div`
   position: absolute;
 
   z-index: 10;
+
+  box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.58);
 `;
 
 const StyledSelectorListComponent = styled.div`
   width: 100%;
-  background-color: #83867F; 
-`;
+  background-color: #83867f;
 
-const StyledSelectorListComponentCurrent = styled(StyledSelectorListComponent)`
-  
+  &:hover {
+    box-shadow: inset 0px 0px 10px 0px #43d9d9;
+  }
 `;
 
 export const Selector = ({ text }) => {
   const [isListOpen, setIsListOpen] = useState(false);
 
-  if (isListOpen) window.addEventListener("click", e => {
-    if (!(e.target.id.valueOf() === "selector")) {
-      setIsListOpen(false);
-    }
-  })
+  if (isListOpen)
+    window.addEventListener("click", (e) => {
+      if (!(e.target.id.valueOf() === "selector")) {
+        setIsListOpen(false);
+      }
+    });
 
   const renderComponents = (arr = []) => {
-    return arr.map(element => <StyledSelectorListComponentCurrent id="selector">{element}</StyledSelectorListComponentCurrent>)
-  }
+    return arr.map((element) => (
+      <StyledSelectorListComponent id="selector">
+        {element}
+      </StyledSelectorListComponent>
+    ));
+  };
 
   return (
     <StyledSelectorContainer id="selector">
-      <StyledSelectorHeader id="selector" onClick={() => isListOpen ? setIsListOpen(false) : setIsListOpen(true)}>
+      <StyledSelectorHeader
+        id="selector"
+        onClick={() =>
+          isListOpen ? setIsListOpen(false) : setIsListOpen(true)
+        }
+      >
         {text}
       </StyledSelectorHeader>
-      {
-      isListOpen ? 
-        (
-          <StyledSelectorList id="selector" onClick={() => setIsListOpen(false)}>
-            {renderComponents(soundChoices)}
-          </StyledSelectorList>
-        ) : null
-      }
+      {isListOpen ? (
+        <StyledSelectorList id="selector" onClick={() => setIsListOpen(false)}>
+          {renderComponents(soundChoices)}
+        </StyledSelectorList>
+      ) : null}
     </StyledSelectorContainer>
   );
-}
+};
