b4388cd6663087374f87da132ecab470f7401fa1
overhead525 <ritchelcousar@outlook.com>
2021-03-11 08:25:01 -0500

    added a save button

---
+++ src/components/Buttons/SaveButton.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+import { topMenuLineHeight } from "../SynthGUI/TopMenu/_constants";
+
+const StyledCF = styled.div`
+  position: relative;
+
+  width: ${topMenuLineHeight}rem;
+  height: ${topMenuLineHeight}rem;
+
+  margin-left: 0.5rem;
+`;
+
+const StyledSaveButton = styled.img`
+  width: ${topMenuLineHeight}rem;
+
+  position: absolute;
+
+  -webkit-transition: opacity 0.25s ease-in-out;
+  -moz-transition: opacity 0.25s ease-in-out;
+  -o-transition: opacity 0.25s ease-in-out;
+  transition: opacity 0.25s ease-in-out;
+`;
+
+const StyledSaveButtonTop = styled(StyledSaveButton)`
+  &:hover {
+    opacity: 0;
+  }
+`;
+
+export const SaveButton = () => {
+  return (
+    <StyledCF>
+      <StyledSaveButton src="../../assets/save-button-hover.svg" />
+      <StyledSaveButtonTop src="../../assets/save-button.svg" />
+    </StyledCF>
+  )
+}
