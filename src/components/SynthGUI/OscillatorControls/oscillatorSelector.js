717212d3492f924208714566a70e3b79d5955a5e
overhead525 <ritchelcousar@outlook.com>
2021-03-16 09:31:28 -0400

    added assets for pure selctor directional buttons

--- src/components/SynthGUI/OscillatorControls/oscillatorSelector.js
+++ src/components/SynthGUI/OscillatorControls/oscillatorSelector.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { SelectionDirectionalButton } from "../../Buttons/SelectionDirectionalButton";
 
 const StyledOscillatorSelector = styled.div`
   display: flex;
