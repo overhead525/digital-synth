8f0ad7f6896bbe0c0c0cded53ff0e74967421b6f
overhead525 <ritchelcousar@outlook.com>
2021-03-28 18:38:22 -0400

    reset without chartjs

--- src/components/SynthGUI/OscillatorControls/Visualizer/index.js
+++ src/components/SynthGUI/OscillatorControls/Visualizer/index.js
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "preact/hooks";
 import styled from "styled-components";
-import { buildVisualizer } from "./createChart";
 
 const StyledVisualizer = styled.div`
   background-color: rgba(0, 0, 0, 0.8);
@@ -10,25 +8,8 @@ const StyledVisualizer = styled.div`
 `;
 
 export const Visualizer = () => {
-  const [shift, setShift] = useState(0);
-
-  const renderChart = () => {
-    const ctx = document.getElementById("myChart").getContext("2d");
-    const myChart = buildVisualizer(ctx, {
-      xMin: 0,
-      xMax: 50,
-      xStep: 0.2,
-      shift: shift,
-    });
-  };
-
-  useEffect(() => {
-    setTimeout(() => setShift(shift + 1), 300);
-    renderChart();
-  });
-
   return (
-    <StyledVisualizer onClick={() => setShift(shift + 1)}>
+    <StyledVisualizer>
       <canvas id="myChart"></canvas>
     </StyledVisualizer>
   );
