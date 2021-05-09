2e3c5a2d69e5db827b2e3deceefbb0e2df4d3bc6
overhead525 <ritchelcousar@outlook.com>
2021-05-08 20:07:36 -0400

    storing-changes

--- src/components/SynthGUI/OscillatorControls/visualizer.js
+++ src/components/SynthGUI/OscillatorControls/visualizer.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const StyledVisualizer = styled.div`
   background-color: rgba(0, 0, 0, 0.8);
@@ -7,6 +9,49 @@ const StyledVisualizer = styled.div`
   height: 100%;
 `;
 
+const StyledVisualizerContentWrapper = styled.svg`
+  width: 100%;
+  height: 100%;
+`;
+
 export const Visualizer = () => {
-  return <StyledVisualizer></StyledVisualizer>;
+  const checkVariants = {
+    checked: { pathLength: 1 },
+    unchecked: { pathLength: 0 },
+  };
+
+  const [isChecked, setIsChecked] = useState(true);
+  const pathLength = useMotionValue(0);
+  const opacity = useTransform(pathLength, [0.05, 0.15], [0, 1]);
+
+  return (
+    <StyledVisualizer>
+      <StyledVisualizerContentWrapper
+        xmlns="http://www.w3.org/2000/svg"
+        width="300"
+        height="150"
+      >
+        <motion.path
+          d="M10 80  C 40 10, 65 10, 95 80 S 150 150, 180 80"
+          fill="transparent"
+          strokeWidth="20"
+          stroke="#A0D"
+          animate={true}
+          strokeLinecap="round"
+          transition={{
+            repeat: Infinity,
+            type: "tween",
+            duration: 1.5,
+            repeatType: "loop",
+            from: 0.1,
+          }}
+          variants={checkVariants}
+          style={{
+            pathLength: pathLength,
+            opacity: opacity,
+          }}
+        />
+      </StyledVisualizerContentWrapper>
+    </StyledVisualizer>
+  );
 };
